228a98196be8ba60f113200d8ebef4620b5be916
gusVLZ <gvnv.2000@gmail.com>
2020-11-03 00:18:01 -0300

    Remove old

--- src/main/java/br/usjt/what2listen/Controller/SongController.java
+++ src/main/java/br/usjt/what2listen/Controller/SongController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.usjt.what2listen.Repository.SongRepo;
 import br.usjt.what2listen.Model.Song;
 
