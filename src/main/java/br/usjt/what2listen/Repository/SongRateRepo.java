bd2479a83b1f3eac5c0d561b7630ff0c4b478394
Leonardo Goncalves <leonardo.goncalves@gunnebo.com>
2020-11-26 23:37:04 -0300

    commit leo teste

--- src/main/java/br/usjt/what2listen/Repository/SongRateRepo.java
+++ src/main/java/br/usjt/what2listen/Repository/SongRateRepo.java
@@ -17,11 +17,12 @@ public interface SongRateRepo extends CrudRepository<SongRate, Integer> {
             "LEFT JOIN song_x_genre as sxg ON\r\n" + 
 			"sxg.id_song = sg.id\r\n" +
             "LEFT JOIN genre as gr ON\r\n" + 
-			"gr.id = sxg.id_genre\r\n" +             			
+			"gr.id = sxg.id_genre\r\n" +
+			"RIGHT JOIN user_x_genre AS uxg ON\r\n" +
+			"uxg.id_genre = gr.id\r\n" +
+			"WHERE uxg.id_user = 1 AND sr.id_song NOT IN (SELECT id_song FROM song_rate WHERE id_user = 1 GROUP BY id_song)\r\n"+             			
 			"GROUP BY sg.id, sg.name, sg.artist, gr.id, gr.name\r\n" + 
 			"ORDER BY avg(sr.rate) DESC LIMIT 5 ", nativeQuery=true)
 	List<?> getBestRatedSongs();
-
-
 
 }
