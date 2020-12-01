7dd5b8bd00c709e3e7e383595b1ea021f98b3fe1
Leonardo Goncalves <leonardo.goncalves@gunnebo.com>
2020-11-30 21:50:42 -0300

    asd

--- src/main/java/br/usjt/what2listen/Repository/SongGenreRepo.java
+++ src/main/java/br/usjt/what2listen/Repository/SongGenreRepo.java
@@ -15,6 +15,7 @@ public interface SongGenreRepo extends CrudRepository<SongGenre, Integer> {
 
 	@Query(value = "select * from song_x_genre where id_song = ?1", nativeQuery = true)
 	List<SongGenre> getSongGenres(int idSong);
+	
 	@Query(value="select gr.* from song_x_genre as uxg inner join Genre as gr on uxg.id_Genre = gr.id where uxg.id_Song = ?1 and uxg.active=1 and gr.active=1", nativeQuery = true)
     List<Genre> getGenreById(int idSong);
 }
